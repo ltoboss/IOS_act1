43e4fd3f9493b9c987f71a89f7b6ee3b47f9aac4
Alumno <alumno@iMac-de-IT-Admin.local>
2019-01-31 18:41:01 -0600

    -a

---
+++ Playground1.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var v1 = "esto es Swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola mundo " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
+print("Luis Felipe Osuna Tirado")
